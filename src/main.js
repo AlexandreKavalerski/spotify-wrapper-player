24ea91ee5f260e4fb63fe3cb46f1a1ff4b0382bc
Alexandre <kavalerskialexandre@gmail.com>
2017-12-26 14:45:53 -0300

    Removing commented code

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import renderAlbums from './AlbumList';
 const albums = spotify.search.albums('Charlie Brown Jr');
 const albumList = document.getElementById('album-list');
 
-// albums.then(data => console.log(data.albums.items));
 albums.then(data => renderAlbums(data.albums.items, albumList));
 
