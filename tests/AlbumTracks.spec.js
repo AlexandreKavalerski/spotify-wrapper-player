932ec587fb41ff90f184c7e10e0ac04b67275951
Alexandre <kavalerskialexandre@gmail.com>
2017-12-27 16:46:48 -0300

    Add AlbumTracks component with TDD

---
+++ tests/AlbumTracks.spec.js
@@ -0,0 +1,58 @@
+import 'jsdom-global/register';
+import { expect } from 'chai';
+import renderAlbumTracks from '../src/AlbumTracks';
+
+describe('AlbumTracks', () => {
+
+  const data = [{
+    preview_url: 'https://p.scdn.co/mp3-preview/7dd1beef360735002712646fadc8b84174b66692?cid=8897482848704f2a8f8d7c79726a70d4',
+    track_number: 2,
+    name: 'O Côro Vai Comê',
+    duration_ms: 141133,
+  }];
+
+  const data2 = [{
+    preview_url: 'https://p.scdn.co/mp3-preview/7dd1beef360735002712646fadc8b84174b66692?cid=8897482848704f2a8f8d7c79726a70d4',
+    track_number: 2,
+    name: 'O Côro Vai Comê',
+    duration_ms: 141133,
+  },
+  {
+    preview_url: 'https://p.scdn.co/mp3-preview/7dd1beef360735002712646fadc8b84174b66692?cid=8897482848704f2a8f8d7c79726a70d4',
+    track_number: 2,
+    name: 'O Côro Vai Comê',
+    duration_ms: 141133,
+  }];
+
+  const markup = `
+    <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/7dd1beef360735002712646fadc8b84174b66692?cid=8897482848704f2a8f8d7c79726a70d4">
+      <p class="music-number">2</p>
+      <p class="music-title">O Côro Vai Comê</p>
+      <p class="music-duration">141133</p>
+    </div>`;
+
+  const markup2 = `
+    <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/7dd1beef360735002712646fadc8b84174b66692?cid=8897482848704f2a8f8d7c79726a70d4">
+      <p class="music-number">2</p>
+      <p class="music-title">O Côro Vai Comê</p>
+      <p class="music-duration">141133</p>
+    </div>
+    <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/7dd1beef360735002712646fadc8b84174b66692?cid=8897482848704f2a8f8d7c79726a70d4">
+      <p class="music-number">2</p>
+      <p class="music-title">O Côro Vai Comê</p>
+      <p class="music-duration">141133</p>
+    </div>`;
+
+  it('should create and append the markup given a correct data', () => {
+    const element = document.createElement('div');
+    renderAlbumTracks(data, element);
+
+    expect(element.innerHTML).to.be.eql(markup);
+  });
+  it('should create and append the markup when more than 1 item', () => {
+    const element2 = document.createElement('div');
+    renderAlbumTracks(data2, element2);
+
+    expect(element2.innerHTML).to.be.eql(markup2);
+  });
+});
