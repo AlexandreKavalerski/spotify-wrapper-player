e6fe5cf4fad16f1ff7372438305e4a631ddff69f
Alexandre <kavalerskialexandre@gmail.com>
2017-12-27 23:29:47 -0300

    Add SearchTrigger

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import spotify from './Spotify';
-import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import renderAlbumTracks from './AlbumTracks';
+import searchEnterTrigger from './SearchTrigger';
 
-
-const albums = spotify.search.albums('Charlie Brown Jr');
-const albumList = document.getElementById('album-list');
+searchEnterTrigger();
 
 const album = spotify.album.getAlbum('0Dcy3ThOh8LS1qhXUbZWH7');
 const albumInfo = document.getElementById('album-info');
 
 const albumTracks = document.getElementById('album-musics');
-
-albums.then(data => renderAlbums(data.albums.items, albumList));
 
 album
   .then(data => renderAlbumInfo(data, albumInfo))
