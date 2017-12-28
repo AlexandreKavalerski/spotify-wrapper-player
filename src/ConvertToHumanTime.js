477c396e542ef00042fdb08efc675d176e356272
Alexandre <kavalerskialexandre@gmail.com>
2017-12-27 23:15:05 -0300

    Correcting variable declaration

--- src/ConvertToHumanTime.js
+++ src/ConvertToHumanTime.js
@@ -2,6 +2,6 @@ export default function convertToHumanTime(duration) {
   let sec = parseInt((duration / 1000) % 60, 10);
   sec = (sec < 10) ? `0${sec}` : sec;
 
-  let min = parseInt((duration / (1000 * 60)) % 60, 10);
+  const min = parseInt((duration / (1000 * 60)) % 60, 10);
   return `${min}:${sec}`;
 }
